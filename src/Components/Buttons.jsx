14970802ebb4dfd7457eb6f3a789099da1718735
Sameer <sameersattar1111@gmail.com>
2023-08-20 04:54:53 +0500

    heros section  commit

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Buttons = () => {
+const Buttons = ({label , UrlIcon}) => {
   return (
-    <div>Buttons</div>
+    <button className="flex items-center border font-montserrat  leading-none text-lg bg-coral-red border-coral-red  py-4 px-7 rounded-full  text-white gap-2 justify-center ">
+      {label}
+      <img src={UrlIcon} alt="" className='w-5 h-5 rounded-full ml-3' />
+    </button>
   )
 }
 
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { Buttons } from '../Components'
+import { arrowRight } from '../assets/icons'
+import { statistics } from '../constants'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id='home' className="border-2 border-red-500  p-2 w-full flex flex-col md:flex-row justify-center items-center gap-10 max-container min-h-screen">
+      <div className="relative xl:2-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>  Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.</p>
+          <Buttons
+          label="Shop Now"
+          UrlIcon = {arrowRight}
+          />
+          <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
+            {
+              statistics.map((stat) => {
+                return (
+                  <>
+                  <p>{stat.value}</p>
+                  <p>{stat.label}</p>
+                  </>
+                )
+              })
+            }
+          </div>
+      </div>
+    </section>
   )
 }
 
