1c77c3d171ae3651e19088493fce08f8b51475cd
Sameer <sameersattar1111@gmail.com>
2023-08-21 23:53:05 +0500

    specialOffer  commit

--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { offer } from '../assets/images'
+import Buttons from '../Components/Buttons'
+import { arrowRight } from '../assets/icons'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <div className='flex justify-center items-center gap-10 max-container max-xl:flex-col-reverse'>
+      <div className="flex-1">
+        <img src={offer}
+         alt="offer"
+         width={773}
+         height={687}
+         className='object-contain w-full'
+         />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className='text-4xl font-palanquin font-bold'>
+        <span className='text-coral-red'>Special </span>
+          Offer
+        </h2>
+        <p className='mt-4 info-text'> Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.</p>
+        <p className='mt-6 info-text'> Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.</p>
+          <div className="mt-11 flex flex-wrap gap-4">
+            <Buttons label="Shop now" UrlIcon={arrowRight}/>
+            <Buttons label="Learn more"/>
+          </div>
+      </div>
+    </div>
   )
 }
 
