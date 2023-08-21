e9715c427699602ebc0dc4b5362e2cd23e3a957b
Sameer <sameersattar1111@gmail.com>
2023-08-21 22:01:17 +0500

    products  commit

--- src/Components/PopularProductsCard.jsx
+++ src/Components/PopularProductsCard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { star } from '../assets/icons'
 
-const PopularProductsCard = () => {
+const PopularProductsCard = ({imgURL,name,price}) => {
   return (
-    <div>PopularProductsCard</div>
+    <div className='flex flex-col w-full max-sm:w-full'>
+      <img src={imgURL} alt="img" 
+      className='w-[282px] h-[282px]'
+      />
+      <div className="flex justify-start gap-2.5 mt-8">
+        <img src={star} alt="rating" width={28} height={28} />
+          <p className='font-montserrat text-xl leading-normal text-slate-gray'>(4.5)</p>
+      </div>
+      <h3 className='mt-2 text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
+      <p className='mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal'>{price}</p>
+    </div>
   )
 }
 
--- src/Sections/PopularProducts.jsx
+++ src/Sections/PopularProducts.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {products} from "../constants"
+import PopularProductsCard from "../Components/PopularProductsCard"
 
 const PopularProducts = () => {
+  
   return (
-    <div>PopularProducts</div>
+   <section id="products" className='max-conatiner max-sm:mt-12'>
+    <div className="flex flex-col justify-start gap-5">
+      <h2 className='text-4xl font-palanquin font-bold'>Our <span className='text-coral-red'>Popular</span> Products</h2>
+       <p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value</p>
+    </div>
+    <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-14 sm:gap-4">
+      {
+        products.map((product) => {
+          console.log(product)
+          return (
+            <PopularProductsCard key={product.name} {...product}/>
+          )
+        })
+      }
+    </div>
+   </section>
   )
 }
 
